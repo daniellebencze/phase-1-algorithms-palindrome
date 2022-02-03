04c374d96e43636051bda6afbb2edadbf4dc1404
Danielle Bencze <daniellebencze@live.com>
2022-02-02 17:13:28 -0800

    commit tree f4d87178fe00f8c8cd6e0bc8e5d741c82ffdf42f

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let j = str.length - 1;
+  for (let i = 0; i < j / 2; i++) {
+    let x = str[i]; //forward character
+    let y = str[j - i]; //backward character
+    if (x != y) {
+      // return false if string not match
+      return false;
+    }
+  }
+  /// return true if string is palindrome
+  return true;
 }
 
 /* 
